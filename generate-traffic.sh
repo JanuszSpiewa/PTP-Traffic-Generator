002bd5eae669299b04f18ccb161ea1f7ea699187
JanuszSpiewa <eldzik2@gmail.com>
2020-01-30 15:55:02 +0100

    Update generate-traffic.sh

--- generate-traffic.sh
+++ generate-traffic.sh
@@ -6,4 +6,9 @@ tor -f /etc.tor/torrc&
 wait 10
 echo "[INFO] running tor-prompt
 tor-prompt -i 127.0.0.1 9051&
-
+wait 10
+proxychains4 firefox -new-window <url address to open>
+wait 10
+for i in $./generate-traffic.sh; do 
+    echo $i
+done
