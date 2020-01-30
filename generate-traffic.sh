a0cf5350450626542ac4c6b14ebb3cba6007d877
JanuszSpiewa <eldzik2@gmail.com>
2020-01-30 15:47:17 +0100

    Update generate-traffic.sh

--- generate-traffic.sh
+++ generate-traffic.sh
@@ -2,5 +2,8 @@
 
 echo "Please don't use this script. The script is being created and may not work properly and generate errors"
 echo "[INFO] runing tor 
-tor -f /etc.tor/torrc
+tor -f /etc.tor/torrc&
 wait 10
+echo "[INFO] running tor-prompt
+tor-prompt -i 127.0.0.1 9051&
+
