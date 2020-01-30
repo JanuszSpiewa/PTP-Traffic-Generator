ada17ed0c4f0b758af2973522e47fed9f91828aa
JanuszSpiewa <eldzik2@gmail.com>
2020-01-30 15:33:57 +0100

    Create generate-traffic.sh

---
+++ generate-traffic.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+echo "Please don't use this script. The script is being created and may not work properly and generate errors"
+
+tor && tor-prompt& > echo signal newnym && firefox -new-window <url>
